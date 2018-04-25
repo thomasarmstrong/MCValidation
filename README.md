00b8e76f2c9d5e7a4bc16535fc5bd6bfa4819ae8
Thomas Armstrong <thomas.armstrong@physics.ox.ac.uk>
2018-04-25 14:29:30 +0100

    Modified run_simtel to take command line options included example in README

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ get_photons.py reads in a run list and provides the required number of photons e
 
 run_simtel.py - just a script that runs the light emission package and sim_telarray. Also can generate a pixel mask file
 
+example run:
+
+python run_simtel.py --infile runlist.txt --outdir ~/Data/test_run --nevents 1 --runLightEmission --angdist ./angular_distribution.dat
+--distance 100 --camradius 30 --runSimTelarray --nsb 0 --discthresh 0
+
+
 exctract_charge_resolution.py 
 
 python extract_charge_resolution.py --input_path ./d2018-02-09_DynRange_NSB0_mc --max_events 10 -o ./charge_resolution_test.h5 --calibrator HESSIOR1Calibrator -T 1 --plot_cam False --use_true_pe True
--- run_simtel.py
+++ run_simtel.py
@@ -1,8 +1,26 @@
 import os
+import argparse
+import numpy as np
 
-def make_mask(on_pixels):
-    cam_file = open('/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray/cfg/CTA/camera_CHEC-S_GATE.dat', 'r')
-    out_file = open('/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray/cfg/CTA/camera_CHEC-S_GATE_masked.dat', 'w')
+## Hardcoded parameters:
+simtel_path = '/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray'
+corsika_path = '/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/corsika-6990/run'
+lightEmission_path = '/scratch/armstrongt/Workspace/CTA/MCValidation/src/LightEmission-pkg'
+
+## old
+pe = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 19, 22, 25, 29, 33, 39, 44, 51, 59, 68, 79, 91, 104, 120, 138,159, 184, 212, 244, 281, 323, 372, 429, 494, 568, 655, 754, 868, 1000]
+photons = [10025.760117977465, 20051.520235954929, 30077.280353932398, 40103.040471909859, 50128.800589887331,
+           60154.560707864795, 70180.320825842267, 80206.080943819718, 90231.841061797197, 100257.60117977466,
+           120309.12141572959, 140360.64165168453, 160412.16188763944, 190489.44224157184, 220566.72259550422,
+           250644.00294943663, 290747.04342134646, 330850.08389325638, 391004.64460112114, 441133.44519100845,
+           511313.76601685071, 591519.84696067055, 681751.68802246766, 792035.04932021978, 912344.1707359493,
+           1042679.0522696566, 1203091.2141572959, 1383554.8962808903, 1594095.858758417, 1844739.8617078539,
+           2125461.1450112229, 2446285.4687865013, 2817238.5931516676, 3238320.5181067213, 3729582.7638876173,
+           4301051.0906123333, 4952725.4982808679, 5694631.7470112005, 6566872.8772752397, 7559423.1289550085,
+           8702359.7824044414, 10025760.117977465]
+
+
+def make_mask(on_pixels, cam_file, out_file):
     for line in cam_file:
         if line.startswith('Pixel'):
             data = line.strip().split(' ')
@@ -13,48 +31,76 @@ def make_mask(on_pixels):
         else:
             out_file.write(line)
 
-def run_simtel(outfile ='../data/bypass2_enoise.simtel.gz', nsb = 0.02, infile = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/beamed_test.dat.gz'):
-	os.system('/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray/bin/sim_telarray '
-			  '-c /scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray/cfg/CTA/CTA-ULTRA6-SST-GCT-S.cfg '
+def run_simtel(outfile ='../data/bypass2_enoise.simtel.gz', nsb = 0.02, disc_thresh= 230,
+               infile = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/beamed_test.dat.gz'):
+	os.system('%s/bin/sim_telarray '
+			  '-c /%s/cfg/CTA/CTA-ULTRA6-SST-GCT-S.cfg '
 			  '-o %s '
 			  '-C BYPASS_OPTICS=2 '
 			  '-C NIGHTSKY_BACKGROUND=all:%s '
-              '-C discriminator_threshold=230 '
+              '-C discriminator_threshold=%s '
               '-C trigger_pixels=2 '
               '-C trigger_telescopes=1 '
               '-C MIN_PHOTONS=0 '
               '-C MIN_PHOTOELECTRONS=0 '
-			  '-I/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray/cfg/CTA/ '
-			  '-I/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/corsika-6990/run '
-			  '%s' % (outfile, nsb, infile))
+			  '-I%s/cfg/CTA/ '
+			  '-I%s '
+			  '%s' % (simtel_path, simtel_path, outfile, nsb, disc_thresh, infile, simtel_path, corsika_path))
 
-def run_lightemission(events = 3, photons = 10946249, distance = 100, cam_radius = 30, ang_dist = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/ang_dist_2.dat', out_file = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/beamed_test.dat.gz'):
-    os.system('/scratch/armstrongt/Workspace/CTA/MCValidation/src/LightEmission-pkg/ff-1m '
+def run_lightemission(events = 3, photons = 10946249, distance = 100, cam_radius = 30,
+                      ang_dist = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/ang_dist_2.dat',
+                      out_file = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/beamed_test.dat.gz'):
+    os.system('%s/ff-1m '
               '--events %s '
               '--photons %s ' 
               '--distance %s '
               '--camera-radius %s '
               '--angular-distribution %s '
-              '-o %s' % (events, photons, distance, cam_radius, ang_dist, out_file))
+              '-o %s' % (lightEmission_path, events, photons, distance, cam_radius, ang_dist, out_file))
 def main():
-    pe = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 19, 22, 25, 29, 33, 39, 44, 51, 59, 68, 79, 91, 104, 120, 138,
-          159, 184, 212, 244, 281, 323, 372, 429, 494, 568, 655, 754, 868, 1000]
-    photons = [10025.760117977465, 20051.520235954929, 30077.280353932398, 40103.040471909859, 50128.800589887331,
-               60154.560707864795, 70180.320825842267, 80206.080943819718, 90231.841061797197, 100257.60117977466,
-               120309.12141572959, 140360.64165168453, 160412.16188763944, 190489.44224157184, 220566.72259550422,
-               250644.00294943663, 290747.04342134646, 330850.08389325638, 391004.64460112114, 441133.44519100845,
-               511313.76601685071, 591519.84696067055, 681751.68802246766, 792035.04932021978, 912344.1707359493,
-               1042679.0522696566, 1203091.2141572959, 1383554.8962808903, 1594095.858758417, 1844739.8617078539,
-               2125461.1450112229, 2446285.4687865013, 2817238.5931516676, 3238320.5181067213, 3729582.7638876173,
-               4301051.0906123333, 4952725.4982808679, 5694631.7470112005, 6566872.8772752397, 7559423.1289550085,
-               8702359.7824044414, 10025760.117977465]
-    for p in range(len(pe)):
-        infl = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/beamed_events1000_pe%s_dist100_camr30_angdistfullflat.dat.gz' % pe[p]
-        outfl = '/scratch/armstrongt/Workspace/CTA/MCValidation/data/bypass2_enoise_pe%s_masked.simtel.gz' % pe[p]
-        print("@@@@ Running LightEmission Package\n\n")
-        run_lightemission(events = 1000, photons = photons[p], out_file = infl)
-        # print("@@@@ Running Simtelarray\n\n")
-        # run_simtel(infile = infl,outfile = outfl)
+
+    # cam_file = open('/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray/cfg/CTA/camera_CHEC-S_GATE.dat', 'r')
+    # out_file = open('/scratch/armstrongt/Software/CTA/CorsikaSimtel/2017-12-08_testing/sim_telarray/cfg/CTA/camera_CHEC-S_GATE_masked.dat', 'w')
+
+    parser = argparse.ArgumentParser(description='Run LightEmission and simtel')
+    parser.add_argument('--infile', default='./runlist.txt', help='File containing run number, Npe and Nphotons')
+    parser.add_argument('--outdir', default= '/scratch/armstrongt/Workspace/CTA/MCValidation/data')
+    parser.add_argument('--nevents', default=1, help='Number of events to run at each illumination')
+    parser.add_argument('--runLightEmission', type=bool, action='store_true', default=False, help='Run Light Emission Package')
+    parser.add_argument('--angdist', default='/scratch/armstrongt/Workspace/CTA/MCValidation/data/ang_dist_2.dat',
+                        help='file containing the angular distribution of the light source')
+    parser.add_argument('--distance', default='100', help='distance of lightsource from detector [cm]')
+    parser.add_argument('--camradius', default='30', help='radius of the fiducial sphere that contains the detector [cm]')
+    parser.add_argument('--runSimTelarray', type=bool, action='store_true', default=False, help='Run Simtelarray')
+    parser.add_argument('--cfg', default='/%s/cfg/CTA/CTA-ULTRA6-SST-GCT-S.cfg' % simtel_path, help='sim_telarray configuration file')
+    parser.add_argument('--nsb', default=0, help='level of non-pulsed background light [MHz]')
+    parser.add_argument('--discthresh', default=0, help='level of discriminator threshold, 0 for external trigger')
+    parser.add_argument('--only_pixels', default=None, help='list of pixels to have turned on')
+
+    args = parser.parse_args()
+
+    runN    = None
+    pe      = None
+    photons = None
+    try:
+        infile  = np.loadtxt(args.infile, unpack=True)
+        runN    = infile[0]
+        pe      = infile[1]
+        photons = infile[2]
+    except FileNotFoundError:
+        print('No such input file, please specify one with --infile FILE')
+
+    for n, p in enumerate(pe):
+        infl = '%s/corsika/run%04d.corsika.gz' % (args.outdir, int(runN[n]))
+        outfl = '%s/sim_tel/run%04d.simtel.gz' % (args.outdir, int(runN[n]))
+        if args.runLightEmission:
+            print("@@@@ Running LightEmission Package\n\n")
+            run_lightemission(events=args.nevents, photons = photons[n],
+                              out_file=infl, ang_dist=args.angdist,
+                              distance=args.distance, cam_radius=args.camradius,)
+        if args.runSimTelarray:
+            print("@@@@ Running Simtelarray\n\n")
+            run_simtel(infile = infl,outfile = outfl, nsb=args.nsb, disc_thresh=args.discthresh)
 
 if __name__ == '__main__':
     main()
