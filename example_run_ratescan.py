c4834374f5dc89d7677c2c75e88bd6137664f331
Thomas Armstrong <thomas.armstrong@physics.ox.ac.uk>
2018-06-15 11:38:08 +0100

    rate scan

--- example_run_ratescan.py
+++ example_run_ratescan.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("python run_simtel.py --infile /home/armstrongt/Workspace/CTA/MCValidation/data/d2018-05-14_100mV_fw50pe/runlist_update2.txt "
-          " --outdir /home/armstrongt/Workspace/CTA/MCValidation/data/d2018-05-14_100mV_fw50pe/mcUpdate2 "
+os.system("python run_simtel.py --infile /home/armstrongt/Workspace/CTA/MCValidation/data/d2018-05-14_100mV_fw50pe/runlist_update2_dummy.txt "
+          " --outdir /home/armstrongt/Workspace/CTA/MCValidation/data/d2018-05-14_100mV_fw50pe/mcUpdate_dumy2 "
           " --nevents 100 "
           " --angdist configs/ang_dist_mpik_flat_20deg.dat "
           " --distance 155.2 "
