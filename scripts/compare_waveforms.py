281b78effb68f4db4ee683c482abb5968fe02b71
Thomas Armstrong <thomas.armstrong@physics.ox.ac.uk>
2018-06-26 11:35:14 +0100

    compare waveforms can now just show individual file data

--- scripts/compare_waveforms.py
+++ scripts/compare_waveforms.py
@@ -7,8 +7,8 @@ import argparse
 import numpy as np
 
 parser = argparse.ArgumentParser(description='Run LightEmission and simtel')
-parser.add_argument('--mcfile', help='Path to mc data file')
-parser.add_argument('--labfile', help='Path to lab data file')
+parser.add_argument('--mcfile', default=None, help='Path to mc data file')
+parser.add_argument('--labfile', default=None, help='Path to lab data file')
 parser.add_argument('--maxevents', default=1, help='maximum number of events to include')
 parser.add_argument('--mc_pix', default=None, type=int, help='specify pixel number to plot in MC data file, otherwise loop over 64')
 parser.add_argument('--lab_pix', default=None, type=int, help='specify pixel number to plot in LAB data file, otherwise loop over 64')
@@ -20,40 +20,52 @@ args = parser.parse_args()
 
 fig0 = plt.figure(0)
 # fig1 = plt.figure(1)
-ax1 = fig0.add_subplot(121)
-ax2 = fig0.add_subplot(122)
+if args.mcfile is None:
+    ax1 = fig0.add_subplot(111)
+    ax1.set_title('Lab data')
+    ax1.set_ylabel('amplitude')
+    ax1.set_xlabel('time [ns]')
+if args.labfile is None:
+    ax2 = fig0.add_subplot(111)
+    ax2.set_title('MC data')
+    ax2.set_ylabel('amplitude')
+    ax2.set_xlabel('time [ns]')
+if args.labfile is not None and args.mcfile is not None:
+    ax1 = fig0.add_subplot(121)
+    ax2 = fig0.add_subplot(122)
 
-ax1.set_title('Lab data')
-ax1.set_ylabel('amplitude')
-ax1.set_xlabel('time [ns]')
+    ax1.set_title('Lab data')
+    ax1.set_ylabel('amplitude')
+    ax1.set_xlabel('time [ns]')
 
-ax2.set_title('MC data')
-ax2.set_ylabel('amplitude')
-ax2.set_xlabel('time [ns]')
+    ax2.set_title('MC data')
+    ax2.set_ylabel('amplitude')
+    ax2.set_xlabel('time [ns]')
 
 ######### CAMERA DATA p.e. ###########
 cal = CameraCalibrator()
 try:
-    for event_r1 in tqdm(event_source(args.labfile, max_events=args.maxevents), total = args.maxevents):
-        cal.calibrate(event_r1) # Not needed as it is already R1 data?
-        print('\n\nCamera Event\n\n')
-        for tel in event_r1.r1.tels_with_data:
-            geom = event_r1.inst.subarray.tel[tel].camera
-            if args.max_pix==None:
-                args.max_pix = len(geom.pix_x)
-            if args.lab_pix == None:
-                for i in range(0,args.max_pix):
-                    if args.lab_thresh==None:
-                        ax1.plot(range(len(event_r1.r1.tel[tel].waveform[0][0])), event_r1.r1.tel[tel].waveform[0][i]/args.mv2pe,
-                                 color='C0')
-                    else:
-                        if max(event_r1.r1.tel[tel].waveform[0][i])/args.mv2pe > args.lab_thresh:
-                            print('plotting only lab pixel: ', i)
-                            ax1.plot(range(len(event_r1.r1.tel[tel].waveform[0][0])), event_r1.r1.tel[tel].waveform[0][i]/args.mv2pe,color='C0')
-            else:
-                ax1.plot(range(len(event_r1.r1.tel[tel].waveform[0][0])),
-                         event_r1.r1.tel[tel].waveform[0][args.lab_pix] / args.mv2pe,
-                         color='C0')
+    if args.labfile is not None:
+        for event_r1 in tqdm(event_source(args.labfile, max_events=args.maxevents), total = args.maxevents):
+            cal.calibrate(event_r1) # Not needed as it is already R1 data?
+            print('\n\nCamera Event\n\n')
+            for tel in event_r1.r1.tels_with_data:
+                geom = event_r1.inst.subarray.tel[tel].camera
+                if args.max_pix==None:
+                    args.max_pix = len(geom.pix_x)
+                if args.lab_pix == None:
+                    for i in range(0,args.max_pix):
+                        if args.lab_thresh==None:
+                            ax1.plot(range(len(event_r1.r1.tel[tel].waveform[0][0])), event_r1.r1.tel[tel].waveform[0][i]/args.mv2pe,
+                                     color='C0')
+                        else:
+                            if max(event_r1.r1.tel[tel].waveform[0][i])/args.mv2pe > args.lab_thresh:
+                                print('plotting only lab pixel: ', i)
+                                ax1.plot(range(len(event_r1.r1.tel[tel].waveform[0][0])), event_r1.r1.tel[tel].waveform[0][i]/args.mv2pe,color='C0')
+                else:
+                    ax1.plot(range(len(event_r1.r1.tel[tel].waveform[0][0])),
+                             event_r1.r1.tel[tel].waveform[0][args.lab_pix] / args.mv2pe,
+                             color='C0')
 
 except FileNotFoundError:
     print('LAB file_not_found')
@@ -61,24 +73,25 @@ except FileNotFoundError:
 ######### MC DATA p.e. ###########
 cal2 = CameraCalibrator()
 try:
-    for event_mc2 in tqdm(event_source(args.mcfile, max_events=args.maxevents), total=args.maxevents):
-        cal2.calibrate(event_mc2)
-        print('\n\nMonte Carlo Event\n\n')
-        for tel in event_mc2.r1.tels_with_data:
-            geom = event_mc2.inst.subarray.tel[tel].camera
-            if args.max_pix==None:
-                args.max_pix = len(geom.pix_x)
-            if args.mc_pix == None:
-                for i in range(0,args.max_pix ):
-                    if args.mc_thresh == None:
-                        ax2.plot(range(len(event_mc2.r1.tel[tel].waveform[0][0])), event_mc2.r1.tel[tel].waveform[0][i],color='C1')
-                    else:
-                        if max(event_mc2.r1.tel[tel].waveform[0][i])>args.mc_thresh:
-                            print('plotting only mc pixel: ', i)
+    if args.mcfile is not None:
+        for event_mc2 in tqdm(event_source(args.mcfile, max_events=args.maxevents), total=args.maxevents):
+            cal2.calibrate(event_mc2)
+            print('\n\nMonte Carlo Event\n\n')
+            for tel in event_mc2.r1.tels_with_data:
+                geom = event_mc2.inst.subarray.tel[tel].camera
+                if args.max_pix==None:
+                    args.max_pix = len(geom.pix_x)
+                if args.mc_pix == None:
+                    for i in range(0,args.max_pix ):
+                        if args.mc_thresh == None:
                             ax2.plot(range(len(event_mc2.r1.tel[tel].waveform[0][0])), event_mc2.r1.tel[tel].waveform[0][i],color='C1')
-            else:
-                ax2.plot(range(len(event_mc2.r1.tel[tel].waveform[0][0])), event_mc2.r1.tel[tel].waveform[0][args.mc_pix],
-                         color='C1')
+                        else:
+                            if max(event_mc2.r1.tel[tel].waveform[0][i])>args.mc_thresh:
+                                print('plotting only mc pixel: ', i)
+                                ax2.plot(range(len(event_mc2.r1.tel[tel].waveform[0][0])), event_mc2.r1.tel[tel].waveform[0][i],color='C1')
+                else:
+                    ax2.plot(range(len(event_mc2.r1.tel[tel].waveform[0][0])), event_mc2.r1.tel[tel].waveform[0][args.mc_pix],
+                             color='C1')
 
 except FileNotFoundError:
     print('MC file not found')
