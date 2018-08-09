2ff63891702c0d653bdcb9010e90f4eda9c64043
Thomas Armstrong <thomas.armstrong@physics.ox.ac.uk>
2018-08-09 14:25:49 +0100

    modifying

--- scripts/trace_photons.py
+++ scripts/trace_photons.py
@@ -20,16 +20,16 @@ z2 = 0
 for line in logfile:
     if line.startswith('Starting position w.r.t. camera front:'):
         data = line.split(' ')[5].split('=')[1].split(',')
-        x1 = float(data[0])
+        z1 = float(data[0])
         y1 = float(data[1])
-        z1 = float(data[2])
+        x1 = float(data[2])
     elif line.startswith('Distance of emission point'):
         z0 = float(line.split(' ')[5])
     elif line.startswith('Line-plane intersection at '):
         data = line.split('(')[1].split(')')[0].split(',')
-        x2 = float(data[0])
+        z2 = float(data[0])
         y2 = float(data[1])
-        z2 = float(data[2])
-        ax0.plot([0,x1], [0,y1], [z0,z1], color='r')
+        x2 = float(data[2])
+        # ax0.plot([0,x1], [0,y1], [z0,z1], color='r')
         ax0.plot([x1,x2], [y1,y2], [z1,z2], color='r')
         plt.pause(0.001)
